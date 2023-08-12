eb9670d5bb56db4cb3976be2f9ea109e5ce830f5
ionicthor <ioniccompound6@gmail.com>
2023-08-12 11:28:08 +0530

    secon attempt

--- src/App.js
+++ src/App.js
@@ -25,22 +25,22 @@ function App() {
       }, 1500);
   }
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success");
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success");
-    }
-  }
+  // const toggleMode = ()=>{
+  //   if(mode === 'light'){
+  //     setMode('dark');
+  //     document.body.style.backgroundColor = '#042743';
+  //     showAlert("Dark mode has been enabled", "success");
+  //   }
+  //   else{
+  //     setMode('light');
+  //     document.body.style.backgroundColor = 'white';
+  //     showAlert("Light mode has been enabled", "success");
+  //   }
+  // }
   return (
     <>
     {/* <Router> */}
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+    {/* <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} /> */}
     <Alert alert={alert}/>
     <div className="container my-3">
     {/* <Switch> */}
